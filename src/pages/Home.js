aa51dfb4bd1580c6065aa6bcd75093e00a1ed703
sashdc <saharshdc@gmail.com>
2024-01-28 19:59:02 -0500

    added homescreen title animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/home.css";
-import anime from 'animejs';
+// import anime from 'animejs';
 
 const Home = () => {
-// title animation
-let textWrapper = document.querySelector(".ml2");
-textWrapper.innerHTML = textWrapper.textContent.replace(
-  /\S/g,
-  "<span class='letter'>$&</span>"
-);
 
-anime
-  .timeline({ loop: true })
-  .add({
-    targets: ".ml2 .letter",
-    scale: [4, 1],
-    opacity: [0, 1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 950,
-    delay: (_, i) => 70 * i,
-  })
-  .add({
-    targets: ".ml2",
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000,
-  });
 
   
   return (
     <div className="main-container">
       <section id="home-page" className="home-page">
         <div className="jumbotron text-center bg-white d-flex flex-column justify-content-between align-middle">
-          <h1 className=" custom-title font-weight-bold ml2">wordplay</h1>
+          <h1 className=" custom-title font-weight-bold tracking-in-expand ">wordplay</h1>
           <div className="subtitle">
             <p className="lead mt-2 ">word.play | \ ˈwərd-ˌplā</p>
             <p className="lead">
