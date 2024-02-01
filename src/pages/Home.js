c320a8ccb3eb493d265d1d662afc1c59f576a0a6
sashdc <saharshdc@gmail.com>
2024-01-31 20:50:05 -0500

    fixed styling responsiveness of homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,14 @@ import "../styles/home.css";
 // import anime from 'animejs';
 
 const Home = () => {
-
-
-  
   return (
     <div className="main-container">
       <section id="home-page" className="home-page">
-        <div className="jumbotron text-center bg-white d-flex flex-column justify-content-between align-middle">
-          <h1 className=" custom-title font-weight-bold tracking-in-expand ">wordplay</h1>
+          <div className=" custom-title ">
+            wordplay
+          </div>
           <div className="subtitle">
-            <p className="lead mt-2 ">word.play | \ ˈwərd-ˌplā</p>
+            <p className="lead  ">word.play | \ ˈwərd-ˌplā</p>
             <p className="lead">
               <i>1. def. playful use of words: verbal wit</i>
             </p>
@@ -21,24 +19,23 @@ const Home = () => {
               <i>2. a game to test and improve your vocabulary</i>
             </p>
           </div>
-          <div className="lead">
+          <div className="lead home-buttons">
             <Link to="/play">
-              <button className="standard-button" id="start-btn">
+              <button className="standard-button home-button" id="start-btn">
                 play
               </button>
             </Link>
             <Link to="/wordbank">
-              <button className="standard-button" id="wb-btn">
+              <button className="standard-button home-button" id="wb-btn">
                 wordbank
               </button>
             </Link>
             <Link to="/rules">
-              <button className="standard-button" id="rules-btn">
+              <button className="standard-button home-button" id="rules-btn">
                 how to play
               </button>
             </Link>
           </div>
-        </div>
       </section>
     </div>
   );
