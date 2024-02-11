71719ffaaffcc3c386e7eec1b1b7cfc0b20481f0
sashdc <saharshdc@gmail.com>
2024-02-11 17:22:21 -0500

    added hover tips for legend

--- src/components/WordbankLegend.js
+++ src/components/WordbankLegend.js
@@ -3,9 +3,9 @@ import React from "react";
 const WordbankLegend = () => {
   return (
     <div className="wordbank-legend">
-      <button className="wordbutton legend-button incomplete">Incomplete</button>
-      <button className="wordbutton legend-button complete-success">Success!</button>
-      <button className="wordbutton legend-button complete-fail">Fail!</button>
+      <button className="wordbutton legend-button incomplete" title= "you didn't complete the round for this word">Incomplete</button>
+      <button className="wordbutton legend-button complete-success" title="you guessed this word correctly!">Success!</button>
+      <button className="wordbutton legend-button complete-fail" title="you got this word wrong">Fail!</button>
 
     </div>
   );
