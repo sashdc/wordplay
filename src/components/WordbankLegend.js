1b7538367d3b514552ea38b19bed44503aec032f
Saharsh DeCruz Cherian <112566088+sashdc@users.noreply.github.com>
2024-02-20 20:16:26 -0500

    added bootstrap tooltips to wordbank legend (#23)

--- src/components/WordbankLegend.js
+++ src/components/WordbankLegend.js
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const WordbankLegend = () => {
+
+  useEffect(() => {
+    // Enable Bootstrap tooltips
+    const tooltips = document.querySelectorAll('[data-toggle="tooltip"]');
+    tooltips.forEach((tooltip) => {
+      new window.bootstrap.Tooltip(tooltip);
+    });
+  }, []);
+
+
   return (
     <div className="wordbank-legend">
       <button
         className="wordbutton legend-button incomplete"
-        title="You didn't complete the round for this word"
+        title="You didn't complete the round for these words"  data-bs-custom-class="dep-tooltip"
+        data-toggle="tooltip"
+        data-placement="bottom"
       >
         Incomplete
       </button>
       <button
         className="wordbutton legend-button complete-success"
-        title="You guessed this word correctly!"
+        title="You guessed these words correctly!"
+        data-bs-custom-class="dep-tooltip"
+        data-toggle="tooltip"
+        data-placement="bottom"
       >
         Success!
       </button>
       <button
         className="wordbutton legend-button complete-fail"
-        title="You got this word wrong"
+        title="You got these words wrong"
+        data-bs-custom-class="dep-tooltip"
+        data-toggle="tooltip"
+        data-placement="bottom"
       >
         Fail!
       </button>
